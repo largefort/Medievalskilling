c37f029ccae08ec82e2af441770efc6300658622
largefort <106451813+largefort@users.noreply.github.com>
2023-08-21 16:27:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -57,14 +57,14 @@ function showTouchNumber(event, touchValue) {
   }
 
   gsap.fromTo(touchNumber, {
-    opacity: 1,
+    opacity: 0,
     y: 1
   }, {
     opacity: 0,
     y: -30,
     duration: 0.5,
     onComplete: function() {
-      touchNumber.style.opacity = "1";
+      touchNumber.style.opacity = "100";
     }
   });
 }
