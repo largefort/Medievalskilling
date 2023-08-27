a97009a8465405226778fef485e7a0802cf7fc89
largefort <106451813+largefort@users.noreply.github.com>
2023-08-27 15:52:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,12 @@ let upgradesOwned = 0;
 let goldCoinsEarned = 0;
 let platform = detectPlatform();
 
-const counter = document.getElementById("counter");
-
 function detectPlatform() {
     const userAgent = navigator.userAgent;
     if (/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile/.test(userAgent)) return "Mobile Device";
     if (/webOS|Windows Phone/.test(userAgent)) return "Web Browser";
-    return "PC";
+    if (/Windows NT|Macintosh|Linux/.test(userAgent)) return "PC";
+    return "Unknown Device";
 }
 
 function clickCastle() {
@@ -28,26 +27,33 @@ function clickCastle() {
 }
 
 function buyUpgrade(type) {
+    let cost = 0;
     switch (type) {
         case "knight":
-            if (coins >= 10) {
-                coins -= 10;
+            cost = 10;
+            if (coins >= cost) {
+                coins -= cost;
                 knightCount++;
                 upgradesOwned++;
+                goldCoinsEarned += cost;
             }
             break;
         case "archer":
-            if (coins >= 25) {
-                coins -= 25;
+            cost = 25;
+            if (coins >= cost) {
+                coins -= cost;
                 archerCount++;
                 upgradesOwned++;
+                goldCoinsEarned += cost;
             }
             break;
         case "wizard":
-            if (coins >= 50) {
-                coins -= 50;
+            cost = 50;
+            if (coins >= cost) {
+                coins -= cost;
                 wizardCount++;
                 upgradesOwned++;
+                goldCoinsEarned += cost;
             }
             break;
     }
@@ -67,13 +73,13 @@ function updateUI() {
     document.getElementById("knight-count").textContent = knightCount;
     document.getElementById("archer-count").textContent = archerCount;
     document.getElementById("wizard-count").textContent = wizardCount;
-    document.getElementById("woodcutting-level").textContent = woodcuttingLevel;
-    document.getElementById("mining-level").textContent = miningLevel;
-    document.getElementById("game-start-time").textContent = new Date(gameStartTime).toLocaleString();
-    document.getElementById("castle-clicks").textContent = castleClicks;
-    document.getElementById("upgrades-owned").textContent = upgradesOwned;
-    document.getElementById("gold-coins-earned").textContent = goldCoinsEarned;
-    document.getElementById("platform").textContent = platform;
+    document.getElementById("woodcutting-level").textContent = `Woodcutting Level: ${woodcuttingLevel}`;
+    document.getElementById("mining-level").textContent = `Mining Level: ${miningLevel}`;
+    document.getElementById("game-start-time").textContent = `Game Started: ${new Date(gameStartTime).toLocaleString()}`;
+    document.getElementById("castle-clicks").textContent = `Castle Clicks: ${castleClicks}`;
+    document.getElementById("upgrades-owned").textContent = `Upgrades Owned: ${upgradesOwned}`;
+    document.getElementById("gold-coins-earned").textContent = `Gold Coins Earned: ${goldCoinsEarned}`;
+    document.getElementById("platform").textContent = `Platform: ${platform}`;
 }
 
 function handleSkillingClick(skill) {
@@ -91,6 +97,7 @@ function handleSkillingClick(skill) {
 function updatePassiveIncome() {
     let totalPassiveIncome = knightCount + archerCount * 2 + wizardCount * 5;
     coins += totalPassiveIncome;
+    goldCoinsEarned += totalPassiveIncome;
     updateUI();
 }
 
