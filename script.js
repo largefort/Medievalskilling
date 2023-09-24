9c018a1282c9910da87a396fdd7d40eb24f6c212
largefort <106451813+largefort@users.noreply.github.com>
2023-09-24 09:42:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const touchNumber = document.getElementById("touch-number");
 var db;
 var request = indexedDB.open("MedievalClickerDB", 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
   db = event.target.result;
   const store = db.createObjectStore("gameState", { keyPath: "id" });
   store.createIndex("value", "value", { unique: false });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
   loadGame();
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
   console.error("Error opening database:", event);
 };
 
@@ -44,21 +44,25 @@ function showTouchNumber(event, touchValue) {
     touchNumber.style.top = `${event.touches[0].clientY - touchNumber.clientHeight}px`;
   }
 
-  gsap.fromTo(touchNumber, {
-    opacity: 1,
-    y: 1
-  }, {
-    opacity: 0,
-    y: -30,
-    duration: 0.5,
-    onComplete: function() {
-      touchNumber.style.opacity = "0";
+  gsap.fromTo(
+    touchNumber,
+    {
+      opacity: 1,
+      y: 1,
+    },
+    {
+      opacity: 0,
+      y: -30,
+      duration: 0.5,
+      onComplete: function () {
+        touchNumber.style.opacity = "0";
+      },
     }
-  });
+  );
 }
 
 function startAutoIncome(type, income) {
-  setInterval(function() {
+  setInterval(function () {
     coins += income;
     updateCounter();
   }, 1000);
@@ -78,33 +82,34 @@ function loadGame() {
   const tx = db.transaction("gameState", "readonly");
   const store = tx.objectStore("gameState");
 
-  store.get("coins").onsuccess = function(event) {
+  store.get("coins").onsuccess = function (event) {
     coins = event.target.result?.value || 0;
     updateCounter();
   };
 
-  store.get("knightCount").onsuccess = function(event) {
+  store.get("knightCount").onsuccess = function (event) {
     knightCount = event.target.result?.value || 0;
     document.getElementById("knight-count").textContent = knightCount;
-    startAutoIncome('knight', knightCount);
+    startAutoIncome("knight", knightCount);
   };
 
-  store.get("archerCount").onsuccess = function(event) {
+  store.get("archerCount").onsuccess = function (event) {
     archerCount = event.target.result?.value || 0;
     document.getElementById("archer-count").textContent = archerCount;
-    startAutoIncome('archer', archerCount * 2);
+    startAutoIncome("archer", archerCount * 2);
   };
 
-  store.get("wizardCount").onsuccess = function(event) {
+  store.get("wizardCount").onsuccess = function (event) {
     wizardCount = event.target.result?.value || 0;
     document.getElementById("wizard-count").textContent = wizardCount;
-    startAutoIncome('wizard', wizardCount * 5);
+    startAutoIncome("wizard", wizardCount * 5);
   };
 
-  store.get("lastSavedTime").onsuccess = function(event) {
+  store.get("lastSavedTime").onsuccess = function (event) {
     const lastSavedTime = event.target.result?.value || Date.now();
     const timeDifference = Date.now() - lastSavedTime;
-    const offlineEarnings = Math.floor(timeDifference / 1000) * (knightCount + archerCount * 2 + wizardCount * 5);
+    const offlineEarnings =
+      Math.floor(timeDifference / 1000) * (knightCount + archerCount * 2 + wizardCount * 5);
     coins += offlineEarnings;
     updateCounter();
   };
@@ -113,7 +118,7 @@ function loadGame() {
 }
 
 function startAutoSave() {
-  setInterval(function() {
+  setInterval(function () {
     saveGame();
   }, 2000);
 }
@@ -130,38 +135,38 @@ function animateCastle() {
     scale: 0.95,
     duration: 0.1,
     yoyo: true,
-    repeat: 1
+    repeat: 1,
   });
 }
 
 function buyUpgrade(type) {
   let cost = 0;
   switch (type) {
-    case 'knight':
+    case "knight":
       cost = 10 * (knightCount + 1);
       if (coins >= cost) {
         coins -= cost;
         knightCount++;
         document.getElementById("knight-count").textContent = knightCount;
-        startAutoIncome('knight', 1);
+        startAutoIncome("knight", 1);
       }
       break;
-    case 'archer':
+    case "archer":
       cost = 20 * (archerCount + 1);
       if (coins >= cost) {
         coins -= cost;
         archerCount++;
         document.getElementById("archer-count").textContent = archerCount;
-        startAutoIncome('archer', 2);
+        startAutoIncome("archer", 2);
       }
       break;
-    case 'wizard':
+    case "wizard":
       cost = 50 * (wizardCount + 1);
       if (coins >= cost) {
         coins -= cost;
         wizardCount++;
         document.getElementById("wizard-count").textContent = wizardCount;
-        startAutoIncome('wizard', 5);
+        startAutoIncome("wizard", 5);
       }
       break;
   }
