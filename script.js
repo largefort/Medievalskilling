4b6146680e9509099deaa9e0336da6cd1bac7147
largefort <106451813+largefort@users.noreply.github.com>
2023-10-13 15:53:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ let archerCount = 0;
 let wizardCount = 0;
 let woodcuttingLevel = 1;
 let miningLevel = 1;
-let knightUpgradeLevel = 0; // New upgrade levels
-let archerUpgradeLevel = 0;
-let wizardUpgradeLevel = 0;
+let paladinCount = 0;
+let passiveIncome = 0;
 let db;
 
-// Function to disable finger zooming
 function disableFingerZooming() {
     document.addEventListener('touchmove', function (event) {
         if (event.scale !== 1) { event.preventDefault(); }
@@ -46,9 +44,7 @@ function saveGameData() {
         wizardCount,
         woodcuttingLevel,
         miningLevel,
-        knightUpgradeLevel, // Save upgrade levels
-        archerUpgradeLevel,
-        wizardUpgradeLevel,
+        paladinCount,
     };
 
     const transaction = db.transaction(["gameState"], "readwrite");
@@ -70,9 +66,7 @@ function loadGameData() {
             wizardCount = savedState.wizardCount;
             woodcuttingLevel = savedState.woodcuttingLevel;
             miningLevel = savedState.miningLevel;
-            knightUpgradeLevel = savedState.knightUpgradeLevel; // Load upgrade levels
-            archerUpgradeLevel = savedState.archerUpgradeLevel;
-            wizardUpgradeLevel = savedState.wizardUpgradeLevel;
+            paladinCount = savedState.paladinCount;
 
             updateUI();
         }
@@ -88,9 +82,9 @@ function updateUI() {
     document.getElementById("wizard-count").textContent = wizardCount;
     document.getElementById("woodcutting-level").textContent = woodcuttingLevel;
     document.getElementById("mining-level").textContent = miningLevel;
-    document.getElementById("knight-upgrade-level").textContent = knightUpgradeLevel; // Update upgrade levels
-    document.getElementById("archer-upgrade-level").textContent = archerUpgradeLevel;
-    document.getElementById("wizard-upgrade-level").textContent = wizardUpgradeLevel;
+    document.getElementById("paladin-count").textContent = paladinCount;
+
+    updatePassiveIncome();
 }
 
 function clickCastle() {
@@ -105,21 +99,28 @@ function buyUpgrade(type) {
             if (coins >= 10) {
                 coins -= 10;
                 knightCount++;
-                knightUpgradeLevel++; // Increase upgrade level
+                updatePassiveIncome(); // Update passive income when buying Knights
             }
             break;
         case "archer":
             if (coins >= 25) {
                 coins -= 25;
                 archerCount++;
-                archerUpgradeLevel++; // Increase upgrade level
+                updatePassiveIncome(); // Update passive income when buying Archers
             }
             break;
         case "wizard":
             if (coins >= 50) {
                 coins -= 50;
                 wizardCount++;
-                wizardUpgradeLevel++; // Increase upgrade level
+                updatePassiveIncome(); // Update passive income when buying Wizards
+            }
+            break;
+        case "paladin":
+            if (coins >= 100) {
+                coins -= 100;
+                paladinCount++;
+                updatePassiveIncome(); // Update passive income when buying Paladins
             }
             break;
     }
@@ -148,54 +149,15 @@ function handleSkillingClick(skill) {
     updateUI();
 }
 
-// Function to calculate and update passive income
 function updatePassiveIncome() {
-    let totalPassiveIncome = knightCount + archerCount * 2 + wizardCount * 5;
+    const totalPassiveIncome = (knightCount + archerCount + wizardCount + paladinCount) * 1; // Adjust the income rate as needed
+    passiveIncome = totalPassiveIncome;
+}
 
-    coins += totalPassiveIncome;
+function earnPassiveIncome() {
+    coins += passiveIncome;
     saveGameData();
     updateUI();
 }
 
-// Function to start passive income updates at intervals (e.g., every second)
-function startPassiveIncome() {
-    setInterval(updatePassiveIncome, 1000);
-}
-
-startPassiveIncome();
-
-// Function to calculate and display offline progress details
-function calculateOfflineProgress() {
-    // Retrieve the last saved timestamp from localStorage
-    const lastSavedTimestamp = localStorage.getItem('lastSavedTimestamp');
-
-    if (lastSavedTimestamp) {
-        const currentTime = new Date().getTime();
-        const elapsedMilliseconds = currentTime - parseInt(lastSavedTimestamp);
-
-        // Calculate progress based on elapsed time (adjust this calculation as needed)
-        const offlineCoinsEarned = Math.floor(elapsedMilliseconds / 1000); // 1 coin per second
-        const offlineWoodcuttingGains = Math.floor(elapsedMilliseconds / 20000); // 1 level every 20 seconds
-        const offlineMiningGains = Math.floor(elapsedMilliseconds / 30000); // 1 level every 30 seconds
-
-        // Display the offline progress details in the modal
-        document.getElementById("offlineGoldEarned").textContent = offlineCoinsEarned;
-        document.getElementById("offlineWoodcuttingLevel").textContent = offlineWoodcuttingGains;
-        document.getElementById("offlineMiningLevel").textContent = offlineMiningGains;
-
-        // Show the offline progress modal
-        document.getElementById("offlineModal").style.display = "block";
-    }
-}
-
-// Function to handle online/offline events
-window.addEventListener('online', () => {
-    // The device is now online, call the updateOfflineProgress function
-    calculateOfflineProgress();
-});
-
-window.addEventListener('offline', () => {
-    // The device is now offline, save the current timestamp
-    const currentTime = new Date().getTime();
-    localStorage.setItem('lastSavedTimestamp', currentTime);
-});
+setInterval(earnPassiveIncome, 100);
