5b62647d6d034a4c8e5da05eb1d9726339c58e4b
jafetegill17 <106451813+largefort@users.noreply.github.com>
2023-12-11 13:46:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,36 +5,44 @@ let wizardCount = 0;
 let woodcuttingLevel = 1;
 let miningLevel = 1;
 let paladinCount = 0;
-let mercenaryCount = 0;
 let passiveIncome = 0;
-let totalClicks = 0;
-let totalCoinsEarned = 0;
-let totalUpgradesPurchased = 0;
-let highestCoinsHeld = 0;
-let totalKnightsRecruited = 0;
-let totalArchersRecruited = 0;
-let totalWizardsRecruited = 0;
-let totalPaladinsRecruited = 0;
-let totalMercenariesRecruited = 0;
-let totalResourcesGathered = 0; // Resource gathering count
-let totalSkillsUpgraded = 0;
 let db;
-let lastSaveTime = Date.now();
+let lastSaveTime = Date.now(); // Initialize lastSaveTime with the current time
 
+// Add an HTML audio element for the upgrade sound
+document.write(`
+<audio id="upgradeSound">
+    <source src="upgradesound.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+</audio>
+`);
+
+// Preload the click sound
 const clickSound = new Audio("click-sound.mp3");
+
+function disableFingerZooming() {
+    document.addEventListener('touchmove', function (event) {
+        if (event.scale !== 1) { event.preventDefault(); }
+    }, { passive: false });
+}
+
+disableFingerZooming();
 
 function initializeDB() {
     const request = indexedDB.open("MedievalClickerDB", 1);
+
     request.onupgradeneeded = function (event) {
         db = event.target.result;
         if (!db.objectStoreNames.contains('gameState')) {
             db.createObjectStore('gameState');
         }
     };
+
     request.onsuccess = function (event) {
         db = event.target.result;
         loadGameData();
     };
+
     request.onerror = function (event) {
         console.log("Error opening DB", event);
     };
@@ -42,12 +50,16 @@ function initializeDB() {
 
 function saveGameData() {
     const gameState = {
-        coins, knightCount, archerCount, wizardCount, woodcuttingLevel, miningLevel,
-        paladinCount, mercenaryCount, passiveIncome, totalClicks, totalCoinsEarned,
-        totalUpgradesPurchased, highestCoinsHeld, totalKnightsRecruited, totalArchersRecruited,
-        totalWizardsRecruited, totalPaladinsRecruited, totalMercenariesRecruited,
-        totalResourcesGathered, totalSkillsUpgraded, lastSaveTime: Date.now()
+        coins,
+        knightCount,
+        archerCount,
+        wizardCount,
+        woodcuttingLevel,
+        miningLevel,
+        paladinCount,
+        lastSaveTime: Date.now(), // Update the last save time
     };
+
     const transaction = db.transaction(["gameState"], "readwrite");
     const store = transaction.objectStore("gameState");
     store.put(gameState, "currentGameState");
@@ -60,6 +72,7 @@ function loadGameData() {
     request.onsuccess = function (event) {
         if (request.result) {
             const savedState = request.result;
+
             coins = savedState.coins;
             knightCount = savedState.knightCount;
             archerCount = savedState.archerCount;
@@ -67,48 +80,49 @@ function loadGameData() {
             woodcuttingLevel = savedState.woodcuttingLevel;
             miningLevel = savedState.miningLevel;
             paladinCount = savedState.paladinCount;
-            mercenaryCount = savedState.mercenaryCount;
-            totalClicks = savedState.totalClicks;
-            totalCoinsEarned = savedState.totalCoinsEarned;
-            totalUpgradesPurchased = savedState.totalUpgradesPurchased;
-            highestCoinsHeld = savedState.highestCoinsHeld;
-            totalKnightsRecruited = savedState.totalKnightsRecruited;
-            totalArchersRecruited = savedState.totalArchersRecruited;
-            totalWizardsRecruited = savedState.totalWizardsRecruited;
-            totalPaladinsRecruited = savedState.totalPaladinsRecruited;
-            totalMercenariesRecruited = savedState.totalMercenariesRecruited;
-            totalResourcesGathered = savedState.totalResourcesGathered;
-            totalSkillsUpgraded = savedState.totalSkillsUpgraded;
-            lastSaveTime = savedState.lastSaveTime;
+            lastSaveTime = savedState.lastSaveTime; // Update the last save time
+
             updateUI();
-            updatePassiveIncome();
         }
     };
 }
 
 initializeDB();
 
+// Function to toggle music
 function toggleMusic() {
     const medievalThemeAudio = document.getElementById("medievaltheme");
-    medievalThemeAudio.paused ? medievalThemeAudio.play() : medievalThemeAudio.pause();
+    if (medievalThemeAudio.paused) {
+        medievalThemeAudio.play();
+    } else {
+        medievalThemeAudio.pause();
+    }
 }
 
+// Function to toggle sound effects
 function toggleSoundEffects() {
     const clickSoundAudio = document.getElementById("click-sound");
-    const upgradeSoundAudio = document.getElementById("upgradesound");
+    const upgradeSoundAudio = document.getElementById("upgradeSound");
+
     clickSoundAudio.muted = !clickSoundAudio.muted;
     upgradeSoundAudio.muted = !upgradeSoundAudio.muted;
 }
 
+// Add event listeners to the checkboxes
 document.getElementById("toggle-music").addEventListener("change", toggleMusic);
 document.getElementById("toggle-sfx").addEventListener("change", toggleSoundEffects);
 
-function compactNumberFormat(num) {
-    if (num < 1000) return num;
-    if (num >= 1000 && num < 1000000) return (num / 1000).toFixed(1) + "K";
-    if (num >= 1000000 && num < 1000000000) return (num / 1000000).toFixed(1) + "M";
-    if (num >= 1000000000 && num < 1000000000000) return (num / 1000000000).toFixed(1) + "B";
-    return (num / 1000000000000).toFixed(1) + "T";
+// Function to request fullscreen
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome and Safari
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // Internet Explorer
+        element.msRequestFullscreen();
+    }
 }
 
 function updateUI() {
@@ -119,126 +133,97 @@ function updateUI() {
     document.getElementById("woodcutting-level").textContent = woodcuttingLevel;
     document.getElementById("mining-level").textContent = miningLevel;
     document.getElementById("paladin-count").textContent = paladinCount;
-    document.getElementById("mercenary-count").textContent = mercenaryCount;
-    updateStatsUI();
-}
 
-function updateStatsUI() {
-    document.getElementById("total-clicks").textContent = `Total Castle Clicks: ${totalClicks}`;
-    document.getElementById("total-coins-earned").textContent = `Total Coins Earned: ${compactNumberFormat(totalCoinsEarned)}`;
-    document.getElementById("total-upgrades-purchased").textContent = `Total Upgrades Purchased: ${totalUpgradesPurchased}`;
-    document.getElementById("highest-coins-held").textContent = `Highest Gold Coins Held: ${compactNumberFormat(highestCoinsHeld)}`;
-    document.getElementById("total-knights-recruited").textContent = `Total Knights Recruited: ${totalKnightsRecruited}`;
-    document.getElementById("total-archers-recruited").textContent = `Total Archers Recruited: ${totalArchersRecruited}`;
-    document.getElementById("total-wizards-recruited").textContent = `Total Wizards Recruited: ${totalWizardsRecruited}`;
-    document.getElementById("total-paladins-recruited").textContent = `Total Paladins Recruited: ${totalPaladinsRecruited}`;
-    document.getElementById("total-mercenaries-recruited").textContent = `Total Mercenaries Recruited: ${totalMercenariesRecruited}`;
-    document.getElementById("total-resources-gathered").textContent = `Total Resources Gathered: ${compactNumberFormat(totalResourcesGathered)}`;
-    document.getElementById("total-skills-upgraded").textContent = `Total Skills Upgraded: ${totalSkillsUpgraded}`;
+    updatePassiveIncome();
 }
 
 function clickCastle() {
     coins++;
-    totalClicks++;
-    totalCoinsEarned++;
-    if (coins > highestCoinsHeld) {
-        highestCoinsHeld = coins;
-    }
     saveGameData();
     updateUI();
+
+    // Play the preloaded click sound
     clickSound.play();
 }
 
 function buyUpgrade(type) {
     let cost = 0;
-    let purchased = false;
+    let upgradeCount;
+
     switch (type) {
         case "knight":
             cost = 10;
+            upgradeCount = knightCount;
             if (coins >= cost) {
                 coins -= cost;
                 knightCount++;
-                totalKnightsRecruited++;
-                purchased = true;
             }
             break;
         case "archer":
             cost = 25;
+            upgradeCount = archerCount;
             if (coins >= cost) {
                 coins -= cost;
                 archerCount++;
-                totalArchersRecruited++;
-                purchased = true;
             }
             break;
         case "wizard":
             cost = 50;
+            upgradeCount = wizardCount;
             if (coins >= cost) {
                 coins -= cost;
                 wizardCount++;
-                totalWizardsRecruited++;
-                purchased = true;
             }
             break;
         case "paladin":
             cost = 100;
+            upgradeCount = paladinCount;
             if (coins >= cost) {
                 coins -= cost;
                 paladinCount++;
-                totalPaladinsRecruited++;
-                purchased = true;
-            }
-            break;
-        case "mercenary":
-            cost = 200;
-            if (coins >= cost) {
-                coins -= cost;
-                mercenaryCount++;
-                totalMercenariesRecruited++;
-                purchased = true;
             }
             break;
     }
-    if (purchased) {
-        totalUpgradesPurchased++;
-        updatePassiveIncome();
-        saveGameData();
-        updateUI();
+
+    if (cost > 0) {
+        // Play the upgrade sound
+        const upgradeSound = document.getElementById("upgradeSound");
+        upgradeSound.play();
     }
+
+    saveGameData();
+    updateUI();
+}
+
+function compactNumberFormat(num) {
+    if (num < 1e3) return num;
+    if (num >= 1e3 && num < 1e6) return +(num / 1e3).toFixed(1) + "K";
+    if (num >= 1e6 && num < 1e9) return +(num / 1e6).toFixed(1) + "M";
+    if (num >= 1e9 && num < 1e12) return +(num / 1e9).toFixed(1) + "B";
+    return +(num / 1e12).toFixed(1) + "T";
 }
 
 function handleSkillingClick(skill) {
     switch (skill) {
         case "woodcutting":
             woodcuttingLevel++;
-            totalSkillsUpgraded++;
-            totalResourcesGathered += woodcuttingLevel;
             break;
         case "mining":
             miningLevel++;
-            totalSkillsUpgraded++;
-            totalResourcesGathered += miningLevel;
             break;
     }
-    document.getElementById("resource-gathering-count").textContent = `Total Resources Gathered: ${compactNumberFormat(totalResourcesGathered)}`;
     saveGameData();
     updateUI();
 }
 
 function updatePassiveIncome() {
-    const knightIncomeRate = 1;
-    const archerIncomeRate = 2;
-    const wizardIncomeRate = 4;
-    const paladinIncomeRate = 8;
-    const mercenaryIncomeRate = 16;
+    // Calculate passive income based on knights, archers, wizards, and paladins
+    const knightIncomeRate = 1;   // Adjust the income rate for knights
+    const archerIncomeRate = 2;   // Adjust the income rate for archers
+    const wizardIncomeRate = 4;   // Adjust the income rate for wizards
+    const paladinIncomeRate = 8;  // Adjust the income rate for paladins
 
-    const totalPassiveIncome = (
-        knightCount * knightIncomeRate +
-        archerCount * archerIncomeRate +
-        wizardCount * wizardIncomeRate +
-        paladinCount * paladinIncomeRate +
-        mercenaryCount * mercenaryIncomeRate
-    );
+    const totalPassiveIncome = (knightCount * knightIncomeRate + archerCount * archerIncomeRate + wizardCount * wizardIncomeRate + paladinCount * paladinIncomeRate);
     passiveIncome = totalPassiveIncome;
 }
 
@@ -248,7 +233,7 @@ function earnPassiveIncome() {
     const offlinePassiveIncome = Math.floor(passiveIncome * (timeDifference / 1000));
 
     coins += offlinePassiveIncome;
-    lastSaveTime = currentTime;
+    lastSaveTime = currentTime; // Update the last save time
 
     saveGameData();
     updateUI();
