8025aafefbe4723e98cd1be447b758598c54080c
jafetegill17 <106451813+largefort@users.noreply.github.com>
2023-12-19 17:39:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -275,3 +275,71 @@ document.getElementById("shadows-toggle").addEventListener("change", toggleShado
 document.getElementById("graphics-quality-select").addEventListener("change", function (e) {
     setGraphicsQuality(e.target.value);
 });
+LaggedAPI.init('lagdev_4255', 'ca-pub-4695170235902689');
+//
+//pause game / music before calling:
+//
+LaggedAPI.APIAds.show(function() {
+
+  //
+  // ad is finished, unpause game/music
+  //
+
+console.log("ad completed");
+
+});
+var boardinfo={};
+boardinfo.score=SCORE_VAR;
+boardinfo.board=medievalclicker_hsbdltp;
+
+LaggedAPI.Scores.save(boardinfo, function(response) {
+if(response.success) {
+console.log('high score saved')
+}else {
+console.log(response.errormsg);
+}
+});
+var api_awards=[];
+api_awards.push(medievalclicker_rrgau001 - bought one knight in MedievalClicker);
+
+//can push more than one award at a time
+LaggedAPI.Achievements.save(api_awards, function(response) {
+if(response.success) {
+console.log('achievement saved')
+}else {
+console.log(response.errormsg);
+}
+});
+var api_awards=[];
+api_awards.push(medievalclicker_rrgau002 - bought one archer in MedievalClicker);
+
+//can push more than one award at a time
+LaggedAPI.Achievements.save(api_awards, function(response) {
+if(response.success) {
+console.log('achievement saved')
+}else {
+console.log(response.errormsg);
+}
+});
+var api_awards=[];
+api_awards.push(medievalclicker_rrgau003 - bought one wizard in MedievalClicker);
+
+//can push more than one award at a time
+LaggedAPI.Achievements.save(api_awards, function(response) {
+if(response.success) {
+console.log('achievement saved')
+}else {
+console.log(response.errormsg);
+}
+});
+var api_awards=[];
+api_awards.push(medievalclicker_rrgau004 - bought one paladin in MedievalClicker);
+
+//can push more than one award at a time
+LaggedAPI.Achievements.save(api_awards, function(response) {
+if(response.success) {
+console.log('achievement saved')
+}else {
+console.log(response.errormsg);
+}
+});
